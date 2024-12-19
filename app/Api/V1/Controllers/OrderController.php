3b5dbcbc78db97d40ae6981e13176426ae7da6f8
Pham Ngoc <ngoc.psh@kiotviet.com>
2024-12-19 11:14:35 +0700

    base service

---
+++ app/Api/V1/Controllers/OrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Api\Traits\ApiTrait;
+
+class OrderController
+{
+    use ApiTrait;
+    public function createOrder()
+    {
+    }
+
+    public function getOrder()
+    {
+    }
+
+    public function cancelOrder()
+    {
+    }
+}
